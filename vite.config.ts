514563fb56ad3e67087bd952e4254fa1da580552
degomapreciouslara1-star <degomapreciouslara1@gmail.com>
2026-06-02 22:49:09 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+   base: '/Degoma-s-Portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
